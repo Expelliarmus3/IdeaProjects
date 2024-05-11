9cb173a848e53d8de2530f3d858fac911b24b937
Ahana Sarkar <115629352+Expelliarmus3@users.noreply.github.com>
2024-05-11 14:29:11 +0530

    Version 3

--- Ahana/src/Exercise2.java
+++ Ahana/src/Exercise2.java
@@ -4,6 +4,6 @@ public class Exercise2 {
     public static void main(String[] args) {
         Scanner v = new Scanner(System.in);
         System.out.print("Enter your name and age: ");
-        System.out.println(v.nextLine()+"! and you are "+v.nextInt()+" years old!!!");
+        System.out.println("Nice to meet you "+v.nextLine()+"! and you are "+v.nextInt()+" years old!!!");
     }
 }
